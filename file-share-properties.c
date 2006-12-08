beb0d08789c9fc802535e1b2d64b9eef38b4ebc9
Alexander Larsson <alexl@redhat.com>
2006-12-08 08:21:01 +0000

    Add textdomain boiler plate (#383092) Patch from Ray Strode
    2006-12-08  Alexander Larsson  <alexl@redhat.com>
    
    	* file-share-properties.c: (main):
    	Add textdomain boiler plate (#383092)
    	Patch from Ray Strode

--- file-share-properties.c
+++ file-share-properties.c
@@ -259,6 +259,10 @@ main (int argc, char *argv[])
     GtkCellRenderer *cell;
     GtkTreeIter iter;
     
+    bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+    bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+    textdomain (GETTEXT_PACKAGE);
+    
     gtk_init (&argc, &argv);
 
     ui = glade_xml_new (DATADIR"file-share-properties.glade",
