623a3a3c3516565e91fc9232266727e7a7631ffb
Cosimo Cecchi <cosimoc@gnome.org>
2012-05-21 15:52:32 -0400

    file-share-properties: use g_set_prgname()
    Or the Shell won't pick up the correct menu name.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=676444

--- src/file-share-properties.c
+++ src/file-share-properties.c
@@ -679,6 +679,8 @@ main (int argc, char *argv[])
     bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
     textdomain (GETTEXT_PACKAGE);
 
+    g_set_prgname ("gnome-user-share-properties");
+
     app = gtk_application_new ("org.gnome.user-share.properties",
                                G_APPLICATION_FLAGS_NONE);
     g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
