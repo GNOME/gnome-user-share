8fcc389595caca5da3410a2a0efb9dcff0546ff0
Bastien Nocera <hadess@hadess.net>
2013-02-20 20:58:54 +0100

    build: Rename configure.in to configure.ac

--- autogen.sh
+++ autogen.sh
@@ -7,7 +7,7 @@ test -z "$srcdir" && srcdir=.
 PKG_NAME="gnome-user-share"
 REQUIRED_AUTOMAKE_VERSION=1.6
 
-(test -f $srcdir/configure.in \
+(test -f $srcdir/configure.ac \
   && test -f $srcdir/src/user_share.c) || {
     echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
     echo " top-level $PKG_NAME directory"
