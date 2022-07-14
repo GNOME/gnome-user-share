78f015cd8e23ca8fdf09943b4e77c8d7c6004867
Maximiliano Sandoval R <msandova@gnome.org>
2022-07-14 21:54:24 +0200

    build: Use gnome.post_install

--- meson_post_install.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-import subprocess
-import sys
-
-if not os.environ.get('DESTDIR'):
-    print('Compiling gsettings schemas...')
-    subprocess.call(['glib-compile-schemas', sys.argv[1]])
