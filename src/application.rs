7bffc1118ca2eda17475eaf09577cb38a6494157
Khalid Abu Shawarib <kas@gnome.org>
2024-05-16 14:49:27 +0300

    general: Port to Rust

---
+++ src/application.rs
@@ -0,0 +1,102 @@
+/* application.rs
+ *
+ * Copyright ©2024 The GNOME Project
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * Authors: Khalid Abu Shawarib <kas@gnome.org>
+ *
+ */
+
+use std::process::exit;
+
+use gio::glib;
+use gio::prelude::*;
+use gio::subclass::prelude::*;
+
+use crate::{http, settings::Settings};
+
+const APPLICATION_ID: &str = "org.gnome.user-share.webdav";
+
+mod imp {
+
+	use std::cell::OnceCell;
+
+	use gio::ApplicationHoldGuard;
+
+	use super::*;
+
+	#[derive(Default)]
+	pub struct WebdavApplication {
+		pub holder: OnceCell<ApplicationHoldGuard>,
+	}
+
+	#[gio::glib::object_subclass]
+	impl ObjectSubclass for WebdavApplication {
+		const NAME: &'static str = "WebdavApplication";
+		type Type = super::WebdavApplication;
+		type ParentType = gio::Application;
+	}
+
+	impl ObjectImpl for WebdavApplication {}
+
+	impl ApplicationImpl for WebdavApplication {
+		fn startup(&self) {
+			self.parent_startup();
+
+			let connection = self
+				.obj()
+				.dbus_connection()
+				.expect("Unable to connect to d-bus.");
+			let signal_handler = glib::clone!(
+				#[weak(rename_to = app)]
+				self,
+				#[upgrade_or]
+				glib::ControlFlow::Break,
+				move || {
+					app.obj().quit();
+
+					glib::ControlFlow::Continue
+				}
+			);
+
+			connection.connect_closed(|_, _, _| {
+				http::down();
+
+				exit(2)
+			});
+
+			glib::unix_signal_add_local(libc::SIGINT, signal_handler.clone());
+			glib::unix_signal_add_local(libc::SIGHUP, signal_handler.clone());
+			glib::unix_signal_add_local(libc::SIGTERM, signal_handler);
+
+			Settings::connect_password_required_changed(|_, _| {
+				http::down();
+				http::up();
+			});
+		}
+
+		fn activate(&self) {
+			self.parent_activate();
+
+			http::up();
+
+			// Need to store a reference to the holder
+			self.holder.get_or_init(|| self.obj().hold());
+		}
+	}
+}
+
+glib::wrapper! {
+	pub struct WebdavApplication(ObjectSubclass<imp::WebdavApplication>)
+		@extends gio::Application;
+}
+
+impl Default for WebdavApplication {
+	fn default() -> Self {
+		glib::Object::builder()
+			.property("application-id", APPLICATION_ID)
+			.property("flags", gio::ApplicationFlags::empty())
+			.build()
+	}
+}
---
+++ src/main.rs
@@ -0,0 +1,55 @@
+/* main.rs
+ *
+ * Copyright ©2024 The GNOME Project
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ * Authors: Khalid Abu Shawarib <kas@gnome.org>
+ *
+ */
+
+mod application;
+mod config;
+mod http;
+mod settings;
+
+use gettextrs::{bind_textdomain_codeset, bindtextdomain, textdomain};
+use gio::glib;
+use gio::prelude::*;
+use libc::uid_t;
+
+use application::WebdavApplication;
+use config::{GETTEXT_PACKAGE, LOCALEDIR};
+
+static LOG_DOMAIN: &str = "gnome-user-share: main";
+
+fn main() -> glib::ExitCode {
+	let user_id: uid_t;
+
+	unsafe {
+		user_id = libc::getuid();
+	}
+
+	if user_id == 0 {
+		glib::g_warning!(
+			LOG_DOMAIN,
+			"gnome-user-share cannot be started as root for security reasons."
+		);
+
+		return glib::ExitCode::FAILURE;
+	}
+
+	// Set up gettext translations
+	bindtextdomain(GETTEXT_PACKAGE, LOCALEDIR).expect("Unable to bind the text domain");
+	bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8")
+		.expect("Unable to set the text domain encoding");
+	textdomain(GETTEXT_PACKAGE).expect("Unable to switch to the text domain");
+
+	let app = WebdavApplication::default();
+
+	let result = app.run();
+
+	http::down();
+
+	result
+}
