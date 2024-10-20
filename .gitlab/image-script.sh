84ac07140ca95bd44740313a7249f9cf2dfafd19
Khalid Abu Shawarib <khalid.shawarib@gmail.com>
2024-10-20 23:18:27 +0000

    Rework CI

---
+++ .gitlab/image-script.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+
+dnf builddep -y glib \
+    && git clone --depth 1 https://gitlab.gnome.org/GNOME/glib.git \
+    && cd glib \
+    && meson setup _build -Dintrospection=disabled -Dtests=false --prefix /usr \
+    && ninja -C _build \
+    && ninja install -C _build \
+    && cd .. \
+    && rm -rf glib
+
+dnf builddep -y gobject-introspection \
+    && git clone --depth 1 --recurse-submodules -j8 https://gitlab.gnome.org/GNOME/gobject-introspection.git \
+    && cd gobject-introspection \
+    && meson setup _build --prefix /usr \
+    && ninja -C _build \
+    && ninja install -C _build \
+    && cd .. \
+    && rm -rf gobject-introspection
+
+dnf builddep -y glib \
+    && git clone --depth 1 https://gitlab.gnome.org/GNOME/glib.git \
+    && cd glib \
+    && meson setup _build -Dtests=false --prefix /usr \
+    && ninja -C _build \
+    && ninja install -C _build \
+    && cd .. \
+    && rm -rf glib
